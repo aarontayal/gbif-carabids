777d49e6817da642d8c1d4b2f1f28251050015d5
aarontayal <tayal.14@osu.edu>
2023-09-07 11:03:29 -0400

    Upload stuff
    Uploaded my data, first R script, and some metadata about the dataset

---
+++ Ohio Carabids coding project.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/Aaron/OneDrive/Documents/Entomology MS/Papers books and grants/Carabids/Ohio Carabids dataset")
+carab <- read.table("Ohio_Carabids.csv", sep="\n", header=TRUE, quote="")
+carab_test <- read.table("Ohio_Carabids_test.csv", sep="\t", header=TRUE, quote="")
+
